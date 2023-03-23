bc72abda76becd9cafb37fba8bfb98cf8e54fc56
jdi022222 <jdi022222@gmail.com>
2023-03-23 21:52:56 +0900

    feat: connect to mariaDB for Batch Service

---  Creating a Batch Service/build/resources/main/schema-all.sql
+++  Creating a Batch Service/build/resources/main/schema-all.sql
@@ -1,8 +1,7 @@
-
-DROP TABLE people IF EXISTS;
+DROP TABLE IF EXISTS people;
 
 CREATE TABLE people  (
-                         person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
+                         person_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                          first_name VARCHAR(20),
                          last_name VARCHAR(20)
 );
